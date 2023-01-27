a081121cdc2ce3ce8c5cb2d7daa13cd9c2a8e4ff
Vincent <vincent854321@gmail.com>
2023-01-27 10:59:09 -0600

    Added computer choice and player choice to win and lost statements

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
 const choices = ["rock", "paper", "scissors"];
+const pChoice = playerChoice();
+const cChoice = computerChoice();
 
 function game()
 {
-    console.log("This is the players choice: " + playerChoice());
-    console.log("This is the computers choice: " + computerChoice());
-    verifyGameState();
-
-
+    verifyGameState(pChoice, cChoice);
 }
 
-function verifyGameState()
+
+
+function verifyGameState(pChoice, cChoice)
 {
-    if(computerChoice() === playerChoice())
+    let winConditions =cChoice === "rock" && pChoice === "paper" || cChoice === "paper" && pChoice === "scissors" || cChoice === "scissors" && pChoice === "rock";
+    if(cChoice === pChoice)
     {
-        return console.log("It's a tie!");
+        return console.log("It's a tie!" + " You chose the same throw as the computer.");
     }
-    else if (computerChoice() === "rock" && playerChoice() === "scissors" ||
-                 computerChoice() === "paper" && playerChoice() === "rock" ||
-             computerChoice() === "scissors" && playerChoice() === "paper")
+    else if (winConditions)
     {
-        return console.log("You win!");
+        const winResult = pChoice;
+        const capitalizedWinResult = winResult.charAt(0).toUpperCase() + winResult.slice(1);
+        return console.log("You win! " + capitalizedWinResult + " beats " + cChoice + ".");
     }
-    return console.log("You lost!");
+    const lostResult = cChoice;
+    const capitalizedLostResult = lostResult.charAt(0).toUpperCase() + lostResult.slice(1);
+    return console.log("You lost! " + capitalizedLostResult + " beats " + pChoice + ".");
 }
 
 function computerChoice()
 {
     let input = choices[Math.floor((Math.random() * choices.length))];
-    
+    console.log("This is the computers choice: " + input);
     return input;   
 }
 
 function playerChoice()
 {
     let input = prompt("Choose between Rock, Paper, Scissors?");
+    console.log("This is the players choice: " + input);
     if(choices.includes(input))
     {
         return input.toLowerCase();
     }
     return null;
+    
 }
 
 game();
